62a42adc32be340716a16174e2a4b230645d418e
Dariusz Gołąbek <golabiusz@gmail.com>
2017-02-17 07:16:23 +0100

    Initial commit - symfony3 project + phpunit + doctrine migrations

---
+++ README.md
@@ -0,0 +1,4 @@
+puzzle-detective-app
+====================
+
+A Symfony project created on February 4, 2017, 10:13 pm.
---
+++ src/PuzzleDetective/AppBundle/PuzzleDetectiveAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PuzzleDetective\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PuzzleDetectiveAppBundle extends Bundle
+{
+}
